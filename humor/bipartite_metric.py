f44433fd9db4b8a017e1caa4ecf892a45be77587
ada <ada@gmail.com>
2024-07-22 19:32:47 +0900

    changes

--- humor/bipartite_metric.py
+++ humor/bipartite_metric.py
@@ -2,33 +2,23 @@ import pandas as pd
 from thefuzz import fuzz
 
 def bipartite_metric(model_answers: pd.DataFrame, ground_truth: pd.DataFrame):    
-    scores = model_answers.apply(
+    score_matrix = model_answers.apply(
         lambda row_model: ground_truth.apply(
             lambda row_truth: 
-                fuzz.partial_ratio(row_truth["sentence"], row_model["sentence"]) 
+                fuzz.ratio(row_truth["sentence"], row_model["sentence"]) 
                 if row_model["comedian"] == row_truth["comedian"] 
                 else None,
         axis=1),
     axis=1) \
-        .sub(60) \
-        .clip(lower=0) \
-        .div(40) \
         .melt(ignore_index=False) \
         .dropna() \
-        .reset_index() \
-        .join(model_answers["sentence"], on="index") \
-        .rename(columns={"sentence": "model"}) \
-        .join(ground_truth, on="variable") \
-        .rename(columns={"sentence": "truth", "value": "score"})
-        
-    scores = scores[["comedian", "model", "truth", "score"]]
+        .reset_index()
     
-    df = scores.groupby(["comedian", "model"])["score", "truth"].max().reset_index()
+    best_match = score_matrix.loc[score_matrix.groupby("index")["value"].idxmax()].reset_index(drop=True)
     
-    truths = scores[~scores['truth'].isin(df['truth'])]
-    entries = truths.copy()
-    # entries['score'] = 0
-    df = pd.concat([df, entries[['comedian', 'model', 'truth', 'score']]], ignore_index=True)
-    df.sort_values(by=['comedian', 'model'], inplace=True)
+    missing_gt = set(ground_truth.index) - set(best_match["variable"].unique())
+    missing_gt = pd.DataFrame({"index": None, "variable": list(missing_gt), "value": 0})
     
-    return df.groupby("truth")["score"].mean().reset_index()
+    # TODO: fix case when there are multiple comedians
+    return best_match.append(missing_gt).groupby("variable").mean().rename(columns={"value": "score"})
+    
